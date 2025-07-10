5e4ff9fca91541c4191639de62eb6fe2b987e417
rohansreejith05 <rohansreejith05@gmail.com>
2025-07-10 11:57:08 +0530

    update

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import initialize_agent, AgentType
 from tools import get_packages, create_package
+import streamlit as st
+
+GOOGLE_API_KEY = st.secrets["general"]["GOOGLE_API_KEY"]
+
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="models/gemini-1.5-flash",
     temperature=0,
-    google_api_key=os.getenv("GOOGLE_API_KEY")
+    google_api_key = st.secrets["GOOGLE_API_KEY"]
+
 )
 
 agent = initialize_agent(
