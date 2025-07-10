f7bac994be737b00bc25bf305675e21a23f04b41
rohansreejith05 <rohansreejith05@gmail.com>
2025-07-10 12:44:40 +0530

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 def load_agent():
     llm = ChatGroq(
         groq_api_key=GROQ_API_KEY,
-        model_name="mixtral-8x7b-32768",
+        model_name="llama2-70b-4096",
         temperature=0,
     )
     return initialize_agent(
